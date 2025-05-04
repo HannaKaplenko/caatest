bc5e7ab02b4a6c954e746440a64f735698fea2c5
Hanna <erion777a@gmail.com>
2025-05-04 04:05:21 +0300

    first commit

--- src/authorization/index.tsx
+++ src/authorization/index.tsx
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
 import {styles} from "./styles";
-import { Text,  View,  TextInput, TouchableOpacity, SafeAreaView, Image} from 'react-native';
+import { Text,  View,  TextInput, TouchableOpacity, SafeAreaView, Image, Button} from 'react-native';
 
 export const Authorization = () => {
     const [form, setForm] = useState({
-        login: " ",
-        password: " ",
+        login: "Gazbeton",
+        password: "Boulding",
     });
 
     const OnChangeLogin = (text: string) => {
@@ -15,6 +15,8 @@ export const Authorization = () => {
     const OnChangePassword = (text:string) => {
         setForm ((prev) => ({...prev, password: text,}));
     }
+    const [showPassword, setShowPassword] = useState(false);
+
 
     return (
         <SafeAreaView>
@@ -22,11 +24,35 @@ export const Authorization = () => {
             <View style= {styles.card}>     
             <Text style ={styles.title}>UDK gazbeton</Text>
             <Text style ={styles.label}>Введіть логін</Text>
-           
+            <TextInput
+            value= {form.login}
+            onChangeText={OnChangeLogin}
+            placeholder='Введіть логін'
+            style={styles.input}/>
+        
         
 
     <Text style ={styles.label}>Введіть пароль</Text>
-    <View style= {styles.Image}><Image source= {require("../img/eye.png")} accessibilityLabel="eye"/></View>
+    <View style = {styles.inputWrapper}>
+    <TextInput
+            value= {form.password}
+            onChangeText={OnChangePassword}
+            placeholder='Введіть пароль'
+            secureTextEntry={!showPassword}
+
+            style={styles.input}   
+          />
+          <TouchableOpacity style={styles.eyeButton} onPressIn={() => setShowPassword (!showPassword)}>
+            <Image source= {require("../img/eye.png")} style= {styles.Image} accessibilityLabel="eye"/> 
+            </TouchableOpacity>
+          
+           
+            </View>
+            <Text style= {styles.message}>Якщо ви забули пароль зверніться до адміністратора</Text>
+            
+            <TouchableOpacity style= {styles.button}>
+                <Text style={styles.buttonText}>Увійти</Text></TouchableOpacity>
+                
     </View>
     </View>
         </SafeAreaView>
--- src/authorization/styles.ts
+++ src/authorization/styles.ts
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { Button, StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create ({
     container:{
@@ -17,8 +17,9 @@ borderRadius: 10,
 paddingVertical: 30,
 paddingHorizontal: 24,
 backgroundColor: "#FFFFFF",
-    },
-
+marginHorizontal: 16,
+marginVertical: 195,
+},
     title: {
  color: "#000000",
 fontWeight: 500,
@@ -34,8 +35,60 @@ lineHeight: 32,
 
     Image: {
         width: 16,
-        height: 16,
-        flex: 1,
-        marginLeft: 'auto',
+        height: 11,
+        position: 'absolute',
+        marginLeft: "85%",
+   
+       
+    },
+
+
+    input: {
+        borderRadius: 8,
+        borderWidth: 1,
+        color: "#D8D8DC",
+        marginHorizontal: 8,
+        marginVertical: 10, 
+    },
+
+    inputWrapper: {
+        justifyContent: 'center',
+        display: "flex",
+      },
+
+      eyeButton: {
+        justifyContent: 'center',
+        alignItems: 'center',
+  
+
+      },
+
+    message: {
+        color: "#191F35",
+        fontWeight: "bold",
+        fontSize: 12,
+        textAlign: "center",
+        marginBottom: 40,
+       
+
+    },
+
+    button: {
+        backgroundColor: "#000000",
+        width: 295,
+        height: 44,
+        borderRadius: 8,
+        padding: 10,
+        alignItems: "center",
+        
+    },
+
+    buttonText: {
+        color: "#FFFFFF",
+        fontSize: 16,
+        fontWeight: 'bold',
+
     }
+
+
 })
